ce0e4da87319557420e84a7c20b97e15e40a4498
Mediana Abdelkarim <abdelkarim.mediana@babelgroup.com>
2023-05-30 00:24:33 +0100

    first commit

--- MyIosKeyboard/EmojisViewController.swift
+++ MyIosKeyboard/EmojisViewController.swift
@@ -8,22 +8,99 @@
 import UIKit
 
 class EmojisViewController: UIInputViewController {
+    
+    @IBOutlet private weak var collectionView: UICollectionView!
+    private var dataSource: EmojiDataSource!
+    
+    private var proxy : UITextDocumentProxy {
+        return textDocumentProxy
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        dataSource = EmojiDataSource()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func switchToLetters(_ sender: Any) {
+        self.dismiss(animated: false)
     }
-    */
-
+    
+    
+    @IBAction func deleteText(_ sender: Any) {
+        proxy.deleteBackward()
+    }
+    
 }
+extension EmojisViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dataSource.collectionView(collectionView, numberOfItemsInSection: section)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return dataSource.collectionView(collectionView, cellForItemAt: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("selected")
+    }
+}
+
+
+class EmojiCell: UICollectionViewCell {
+    @IBOutlet  weak var emojiBtn: UIButton!
+    
+    func configure(with emoji: String) {
+        emojiBtn.setTitle(emoji, for: .normal)
+    }
+    
+    @IBAction func selectEmoji(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.4, animations:{
+            sender.transform = CGAffineTransformScale(.identity, 2.0, 2.0)
+        }){_ in
+            sender.transform = CGAffineTransformScale(.identity, 1.0, 1.0)
+        }
+        print(sender.title(for: .normal))
+    }
+    
+}
+
+
+class EmojiDataSource: NSObject, UICollectionViewDataSource {
+    var emojis: [String] = []
+    
+    func getAllEmojis() -> [String] {
+        var emojis: [String] = []
+        for i in 0x1F600...0x1F64F {
+            if let unicodeScalar = UnicodeScalar(i) {
+                let emoji = String(unicodeScalar)
+                emojis.append(emoji)
+            }
+        }
+        return emojis.sorted()
+    }
+    
+    override init() {
+        super.init()
+        self.emojis = self.getAllEmojis()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "EmojiCell", for: indexPath) as! EmojiCell
+        let emoji = emojis[indexPath.item]
+        cell.configure(with: emoji)
+        cell.emojiBtn.addTarget(self, action: #selector(emojiTapped), for: .touchUpInside)
+        return cell
+    }
+    
+    @objc private func emojiTapped() {
+        
+    }
+}
+
--- MyIosKeyboard/KeyboardViewController.swift
+++ MyIosKeyboard/KeyboardViewController.swift
@@ -7,54 +7,174 @@
 
 import UIKit
 
-class KeyboardViewController: UIInputViewController {
+enum Keyboard {
+    case Letters
+    case Chars
+    case CharsII
+    case Emojis
+    case Mic
+}
 
-    @IBOutlet var nextKeyboardButton: UIButton!
+enum Shift {
+    case Maj
+    case Min
+    case FirstChars
+    case SecondChars
+}
+
+
+class KeyboardViewController: UIInputViewController {
     
-    override func updateViewConstraints() {
-        super.updateViewConstraints()
-        
-        // Add custom view sizing constraints here
+    @IBOutlet var keyButtons: [UIButton]!
+    
+    @IBOutlet weak var firstSuggestion: UIButton!
+    @IBOutlet weak var secondSuggestion: UIButton!
+    @IBOutlet weak var thirdSuggestion: UIButton!
+    @IBOutlet weak var suggestionStack: UIStackView!
+    
+    
+    // Variables
+    private var proxy: UITextDocumentProxy {
+        return textDocumentProxy
+    }
+    var chosingKeyboard: Keyboard = .Letters
+    var shiftMode: Shift = .Maj
+    let lettersbutton = [
+                          "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P",
+                          "A", "S", "D", "F", "G", "H", "J", "K", "L",
+                          "⬆", "Z", "X", "C", "V", "B", "N", "M", "⌫",
+                          "123", "😊", "Espace",  "Entrer"
+                        ]
+    
+    let characters1 = [
+                        "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
+                        ",", ".", "?", "!", "'", "\"", "-", "+", "=",
+                        "#+=", "/", "\\", "<", ">", "[", "]", "{","⌫",
+                        "ABC", "😊", "Espace",  "Entrer"
+                      ]
+    
+    let characters2 = [
+                        "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
+                        "}", "(", ")", "#", "%", "^", "*", ":",";",
+                        "#+=", ";", "_", "@", "&", "$", "€", "£","⌫",
+                        "ABC", "😊", "Espace",  "Entrer"
+                      ]
+
+    
+   // Cycle
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let standardKeyboardHeight = self.inputView?.frame.height ?? 0
+        var newFrame = self.view.frame
+        newFrame.size.height = standardKeyboardHeight
+        self.view.frame = newFrame
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Perform custom UI setup here
-        self.nextKeyboardButton = UIButton(type: .system)
-        
-        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
-        self.nextKeyboardButton.sizeToFit()
-        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
-        
-        self.view.addSubview(self.nextKeyboardButton)
-        
-        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
-    }
-    
-    override func viewWillLayoutSubviews() {
-        self.nextKeyboardButton.isHidden = !self.needsInputModeSwitchKey
-        super.viewWillLayoutSubviews()
+        fillLetters(mode: shiftMode)
+        suggestionStack.isHidden = true
     }
     
     override func textWillChange(_ textInput: UITextInput?) {
-        // The app is about to change the document's contents. Perform any preparation here.
+
+        // Called when the text will change
+       
     }
     
     override func textDidChange(_ textInput: UITextInput?) {
-        // The app has just changed the document's contents, the document context has been updated.
-        
-        var textColor: UIColor
-        let proxy = self.textDocumentProxy
-        if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
-            textColor = UIColor.white
-        } else {
-            textColor = UIColor.black
-        }
-        self.nextKeyboardButton.setTitleColor(textColor, for: [])
-    }
+        // Called when the text has changed
+        //suggestionStack.isHidden = false
 
+    }
+    
+    
+    private func fillLetters(mode: Shift){
+        chosingKeyboard = .Letters
+        shiftMode = mode
+        for button in  keyButtons {
+            button.setTitle( mode == .Maj ?  lettersbutton[button.tag].uppercased() : lettersbutton[button.tag].lowercased() , for: .normal)
+        }
+    }
+    
+    private func fillCaracters(mode: Shift){
+        chosingKeyboard = .Chars
+        shiftMode = mode
+        for button in  keyButtons {
+            button.setTitle(mode == .FirstChars ?  characters1[button.tag] : characters2[button.tag] , for: .normal)
+        }
+    }
+    
+    private func getTextFromTextInput(_ textInput: UITextInput?) -> String? {
+        if let textRange = textInput?.selectedTextRange {
+            if let text = textInput?.text(in: textRange) {
+                return text
+            }
+        }
+        return nil
+    }
+    
+    
+    
+   // Action Button
+
+    @IBAction func numbers(_ sender: Any) {
+         if chosingKeyboard == .Letters {
+            fillCaracters(mode: shiftMode)
+        }else{
+            fillLetters(mode: shiftMode == .FirstChars ? .SecondChars : .FirstChars)
+        }
+    }
+    
+    @IBAction func shiftButton(_ sender: Any) {
+     
+        if chosingKeyboard == .Chars {
+            fillCaracters(mode: shiftMode == .FirstChars ? .SecondChars : .FirstChars)
+        }else{
+            fillLetters(mode: shiftMode == .Maj ? .Min : .Maj)
+        }
+    }
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        suggestionStack.isHidden = false
+        UIView.animate(withDuration: 0.2, animations:{
+            sender.transform = CGAffineTransformScale(.identity, 1.2, 1.2)
+        }){_ in
+            sender.transform = CGAffineTransformScale(.identity, 1.0, 1.0)
+        }
+        
+        if chosingKeyboard == .Letters {
+            proxy.insertText(lettersbutton[sender.tag])
+            fillLetters(mode: .Min)
+            if let text = self.proxy.documentContextBeforeInput{
+                firstSuggestion.setTitle(text, for: .normal)
+            }
+        }else{
+            proxy.insertText(shiftMode == .FirstChars ?  characters1[sender.tag] : characters2[sender.tag])
+        }
+    }
+    
+    @IBAction func deleteKeyPressed(_ sender: UIButton) {
+        proxy.deleteBackward()
+    }
+    
+    @IBAction func spaceKeyPressed(_ sender: UIButton) {
+        proxy.insertText(" ")
+    }
+    
+    @IBAction func returnKeyPressed(_ sender: UIButton) {
+        proxy.insertText("\n")
+    }
+    
 }
+ 
+
+
+
+ 
+
